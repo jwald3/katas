ee979c6f9a96c55d050f3d7c83fcc82a33990155
jwald3 <59290280+jwald3@users.noreply.github.com>
2025-03-14 12:42:00 -0400

    Update twosum.cpp with explanatory comments

--- cpp/twosum.cpp
+++ cpp/twosum.cpp
@@ -6,21 +6,25 @@ std::vector<int> twosum(std::vector<int> numbers, int target)
 {
     // define map
     std::map<int, int> numMap {};
+    // numbers.size() isn't natively an integer, it's some weird size type. I'll dig into why
     int length { static_cast<int>(numbers.size())};
 
-    // loop 1
+    // loop 1 - populate that map with the value as the key and the index as the key. I need that index so I can return it later
     for (int i {0}; i < length; i++)
         numMap[numbers[i]] = i;
 
-    // loop 2
+    // loop 2 - loop back through the list and check if the complement for the current number is a key in the map
     for (int j {0}; j < length; j++)
     {
         int complement = target - numbers[j];
 
+        // count will return a value based on if the key is in the map
+        // the complement needs to in the map as a key and it can't be at the same index (if the key is 50% of the goal)
         if (numMap.count(complement) && numMap[complement] != j)
             return { j, numMap[complement] };
     }
 
+    // empty vector if no pairs found
     return {};
 }
 
