e4428779e2c0937e2bb685768951196ab3417a28
Jordan <jwald3@brockport.edu>
2025-03-16 23:47:59 -0400

    Refactor multiplesOfFive function to simplify logic and improve readability; add assertions for validation

--- cpp/multiples_of_five.cpp
+++ cpp/multiples_of_five.cpp
@@ -1,29 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 int multiplesOfFive(int number)
 {
-    if (number <= 0)
-    {
-        return 0;
-    }
-
     int sum {0};
 
-    int i {0};
-
-    while (i < number)
+    // start at 3 because no multiples below that point
+    for (int i {3}; i < number; ++i)
     {
-        if (i % 3 == 0)
-        {
-            sum += i;
-        } 
-        else if (i % 5 == 0)
+        // no need to exclude 15 because this won't double-count it
+        if (i % 3 == 0 || i % 5 == 0)
         {
             sum += i;
         }
-
-        i++;
     }
 
     return sum;
@@ -33,7 +23,9 @@ int main()
 {
     int sums = multiplesOfFive(20);
 
-    std::cout << "The sum of multiples of 3 and 5 that are less than 10 are " << sums << '\n';
+    std::cout << "The sum of multiples of 3 and 5 that are less than 20 are " << sums << '\n';
+    assert(sums == 78);
+
 
     return 0;
 }
