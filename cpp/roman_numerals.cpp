d84d300cafad23cd68a7db7990e8a0da1e6820f7
Jordan <jwald3@brockport.edu>
2025-03-29 15:45:01 -0400

    made solution more idiomatic and added comments

--- cpp/roman_numerals.cpp
+++ cpp/roman_numerals.cpp
@@ -2,10 +2,13 @@
 #include <vector>
 #include <tuple>
 #include <iostream>
+#include <map>
 
 std::string solution(int number)
 {
-    std::vector<std::tuple<int, std::string>> symbols = {
+    // by default, the map will be sorted in ascending order. std::greater<int>
+    // would ensure the map uses descending (by key) ordering.
+    std::map<int, std::string> symbols = {
         {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
         {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"},
         {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
@@ -13,14 +16,17 @@ std::string solution(int number)
 
     std::string result = "";
 
-    for (auto i : symbols) {
-        int value = std::get<0>(i);
-        std::string str = std::get<1>(i); 
-
-        while (number >= value) {
-            result += str;
-
-            number -= value;
+    // `crbegin` and `crend` ensure that the loop goes in descending order.
+    // if it only used `begin` and `end`, it would not be a greedy algorithm because 
+    // it wouldn't start at the highest value first. 
+    // if I used std::greater<int> as the optional third parameter in the map, it could 
+    // be `begin` and `end` instead.
+    for (auto i = symbols.crbegin(); i != symbols.crend();) {
+        if (number >= i->first) {
+            number -= i->first;
+            result += i->second;
+        } else {
+            i++;
         }
     }
 
