86d6abcbbf3896776c43c18f12caf0e2250e02f4
Jordan <jwald3@brockport.edu>
2025-03-30 20:32:20 -0400

    refactored to use the std::accumulate function

--- cpp/better_than_avg.cpp
+++ cpp/better_than_avg.cpp
@@ -1,25 +1,20 @@
 #include <vector>
 #include <iostream>
+#include <numeric>
 
 bool betterThanAverage(std::vector<int> classPoints, int yourPoints)
 {
-    int sum { yourPoints };
-
-    for (int point: classPoints)
-    {
-        sum += point;
-    }
-
-    int average = sum / (static_cast<int>(classPoints.size()) + 1);
-
-    return yourPoints > average;
+    // the numeric package includes the "accumulate" function which works like the JS reduce method to take the items
+    // of a collection and sum them iteratively. 
+    // if this function is not available, a simple for loop would work just as well 
+    return std::accumulate(classPoints.cbegin(), classPoints.cend(), 0) < classPoints.size() * yourPoints;
 }
 
 int main()
 {
-    bool result { betterThanAverage({41, 75, 72, 56, 80, 82, 81, 33}, 50) };
+    bool result { betterThanAverage({100, 40, 34, 57, 29, 72, 57, 88}, 75) };   // should equate to true (1);
 
     std::cout << "Better than average? " << result << '\n';
-
+    
     return 0;
 }
