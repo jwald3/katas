c269484aa3484eb397c49fd513be50522082d3f4
Jordan <jwald3@brockport.edu>
2025-04-01 07:19:15 -0400

    add function to spin words in a string based on length

---
+++ cpp/spin_words.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+std::string spinWords(const std::string &str) {
+    std::vector<std::string> words {};
+
+    std::cout << "Size is " << str.size() << '\n';
+
+    for (size_t i {0}; i < str.size();) {
+        std::string word = "";
+
+        while (str[i] != ' ' && i < str.size()) {
+            word += str[i];
+            ++i;
+        }
+
+        ++i;
+
+        words.push_back(word);
+    }
+
+    std::string spunText {};
+
+    for (size_t i {0}; i < words.size(); ++i) {
+        if (words[i].size() >= 5) {
+            std::reverse(words[i].begin(), words[i].end());
+            spunText += words[i];
+        } else {
+            spunText += words[i];
+        }
+
+        if (words.size() - i > 1) {
+            spunText += ' ';
+        }
+    }
+
+    return spunText;
+}
+
+int main() {
+    std::string spun = spinWords("This is another test");
+
+    std::cout << spun << '\n';
+}
