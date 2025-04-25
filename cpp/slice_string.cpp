6bb919925c66a0a2f0caaf169df7f015f462efbc
Jordan Waldo-Riesel <jwaldo-riesel@indium.com>
2025-04-25 09:27:58 -0400

    added guard condition for sliceString

--- cpp/slice_string.cpp
+++ cpp/slice_string.cpp
@@ -3,6 +3,12 @@
 
 std::string sliceString (std::string str)
 {
+    // the solution involves assuming that the string has at least 3 characters, so this is the
+    // catch to make sure that assumption is valid
+    if (str.size() <= 2) {
+        return "";
+    }
+
     return str.substr(1, str.size() - 2);
 }
 
