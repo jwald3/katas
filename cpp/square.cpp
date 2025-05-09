dd0e50b0f0e63a3e0b70062c616e1654ea5cfd3e
Jordan <jwald3@brockport.edu>
2025-05-09 15:40:10 -0400

    added tests

--- cpp/square.cpp
+++ cpp/square.cpp
@@ -1,4 +1,17 @@
+#include <cassert>
+#include <cmath>
+
 template <typename T>
 T square(const T a) {
     return a * a;
 }
+
+int main() {
+    // cant directly compare floats, so you have to check with epsilon value for 
+    // "close enough" precision
+    assert(fabs(square(3.3) - 10.89) < __FLT_EPSILON__);
+    assert(square(2) == 4);
+
+    return 0;
+}
+
