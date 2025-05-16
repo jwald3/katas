7e2db904f34da5e379c4eefa4499de8b29b13334
Jordan <jwald3@brockport.edu>
2025-05-15 22:24:09 -0400

    added comments

--- cpp/reverse_letters.cpp
+++ cpp/reverse_letters.cpp
@@ -4,6 +4,8 @@ std::string reverse_letter(const std::string &str)
 {
     std::string res = "";
   
+    // you can't do size_t (the natural type for str.size()) because `-1` overflows. 
+    // cast to int to allow a case where `i >= 0` will eventually be false
     for (int i = int(str.size()) - 1; i >= 0; i--) {
       char c = str[i];
       if (c >= 'a' && c <= 'z') {
